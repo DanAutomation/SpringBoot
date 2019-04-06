75961f531343b6aabed7bfef9376f286d9f8f932
dzuiev <dzuiev@playtika.com>
2019-04-06 12:00:13 +0300

    first commit, creation simple project

---
+++ src/README.txt
@@ -0,0 +1,2 @@
+video:
+https://www.youtube.com/watch?v=jH17YkBTpI4&list=PLU2ftbIeotGpAYRP9Iv2KLIwK36-o_qYk
---
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -0,0 +1,26 @@
+package com.example.sweater;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/mappingTest")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, String> model) {
+        model.put("some", "greeting text for some is here");
+        return "main";
+    }
+
+}
