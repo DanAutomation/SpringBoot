7701b7eb1a2ee96903199acc7fb19c02a6264649
dzuiev <dzuiev@playtika.com>
2019-04-07 02:08:47 +0300

    first commit, creation simple project

---
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.example.sweater.repos;
+
+import java.util.List;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepo  extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
